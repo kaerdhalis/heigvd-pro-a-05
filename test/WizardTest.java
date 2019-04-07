16f9ddeaaa58fceed4bd7a3265bbacc82669208f
Guillaume Vetter <guillaume.vetter@epfl.ch>
2019-04-07 16:50:46 +0200

    wizard makes unique id

--- test/WizardTest.java
+++ test/WizardTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class WizardTest {
+
+    @Test
+    public void wizardMakesUniqueId(){
+        Wizard w1 = new Wizard();
+        Wizard w2 = new Wizard();
+        assertNotEquals(w1.getId(), w2.getId());
+    }
+
+
 }
