3ea4aaebfd24d6fa7b6b184c8929f1e38c52fc69
JulienHuguet <julien.huguet@heig-vd.ch>
2019-05-06 08:52:08 +0200

    Final client server

---
+++ server/src/main/java/server/Protocol.java
@@ -0,0 +1,12 @@
+package server;
+
+public class Protocol {
+    public static final int PRESENCE_DEFAULT_PORT = 8384;
+
+    public static final String CMD_FIRE = "FIRE";
+    public static final String CMD_WATER = "WATER";
+    public static final String CMD_EARTH = "EARTH";
+    public static final String CMD_ELECTRICITY = "ELEC";
+    public static final String CMD_EXIT = "EXIT";
+}
+
